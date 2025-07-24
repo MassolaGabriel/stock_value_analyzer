6962fc04121e3825b2cd640f34c57793989883af
Gabriel Massola <154900620+MassolaGabriel@users.noreply.github.com>
2025-07-24 13:29:57 -0300

    Add files via upload

--- src/dao/AcaoDAO.java
+++ src/dao/AcaoDAO.java
@@ -13,10 +13,10 @@ public class AcaoDAO {
     private ConnectionSQL ConnectMySQL;
     private Connection connection = ConnectMySQL.getConnection();
 
-    public ArrayList<Acao> listar() {
+    public ArrayList<Acao> listarAcoes() {
         try {
             ArrayList<Acao> acoes = new ArrayList<>();
-            String sql = "SELECT * FROM acaovalida;";
+            String sql = "SELECT * FROM todasAcoes;";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
 
@@ -38,10 +38,22 @@ public class AcaoDAO {
         return null;
 
     };
-    //    public Boolean cadastrar(Acao acao){}
+        public Boolean cadastrar(Acao acao){
+            try {
+                String sql = "INSERT INTO todasAcoes (setor, ticker, precoAtual, mediaDivCincoAnos) VALUES (?, ?, ?, ?);";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setString(1, acao.getSetor());
+                ps.setString(2, acao.getTicker());
+                ps.setDouble(3, acao.getPrecoAtual());
+                ps.setDouble(4, acao.getMediaDivCincoAnos());
+            }catch (SQLException e ) {
+                System.out.println("Erro ao cadastrar nova ação." + e.getMessage());
+            }
+            return null;
+        }
     //    public Boolean atualizar(Acao acao){};
-    //  public Boolean buscarPeloTicker(Acao acao){};
-    //public Boolean removerAcao(Acao acao){};
+    //    public Boolean buscarPeloTicker(Acao acao){};
+    //    public Boolean removerAcao(Acao acao){};
     //    public Boolean remover (String ticker){};
 
 }
